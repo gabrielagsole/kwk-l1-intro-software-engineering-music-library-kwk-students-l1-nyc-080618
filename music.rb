a67b24731aae38d00d1d5c3526f30ec0d3b98e81
gabrielagsole <ggarciasole@gmail.com>
2018-08-10 18:47:23 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -1,7 +1,3 @@
-
-#1. Define a method named `list_artists`. This method should use the `each_key` method to iterate over the hash and return all the artists (the keys)
-# This method should accept a hash as an argument.
-
 music_library = {
   :taylor_swift => "Shake it Off",
   :cardi_b => "I Like it",
