f9f893bf42f37d1153692b01ad4c668e266a11e7
gabrielagsole <ggarciasole@gmail.com>
2018-08-10 18:47:06 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -6,10 +6,12 @@ music_library = {
   :taylor_swift => "Shake it Off",
   :cardi_b => "I Like it",
   :selena_gomez => "Back To You",
-  
+  :the_killers => "Mr. Brightside"
 }
 
-def list_artists
+def list_artists(music_library)
+  music_library.each_key do |key|
+    puts "#{key}"
 end 
 
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
